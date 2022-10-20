a5b79dd2c9d7d39af999c72d2adef8bcfbcb6249
Katerine Witkoski <ka.lwitkoski@gmail.com>
2022-10-19 22:19:00 -0300

    usando o print

--- anotacoes-slides.md
+++ anotacoes-slides.md
@@ -1,5 +1,7 @@
 # Linguagem Python - Criado por Guido (em 1991).
 
+- variáveis: O que eu quiser guardar durante a execução de um programa eu guardo em 
+uma variável. - Eu não preciso declarar a variável. 
 
 
 
--- aulas-praticas.py
+++ aulas-praticas.py
@@ -3,5 +3,23 @@
 # print() = comando de saída
 print("Hello world!")
 
+# Variável: (string)
+variavel_string_nome = "Katerine Witkoski"
+variavel_inteira_idade = 29
 
+# Exibir o meu nome (que está dentro da variável)
+print(variavel_string_nome)
+print(variavel_inteira_idade)
 
+# Quando quiser exibir a frase "Minha idade é "  completando
+# com o conteúdo da variável idade
+
+print("Minha idade é "+str(variavel_inteira_idade)+" anos") # concatenação (precisa ser da mesma classe)
+print(f"Minha idade é {variavel_inteira_idade}") # precisa colocar o f de formatação de valores
+print("Minha idade é {}".format(variavel_inteira_idade)) # .format = formatação de valores
+print("Minha idade é", variavel_inteira_idade) # funciona também
+
+# Quando quiser exibir "Meu nome é ..X. e tenho ...
+# anos ..."  trocando pelas variáveis nome e idade
+
+print("Meu nome é {} e tenho {} anos".format(variavel_string_nome, variavel_inteira_idade))
