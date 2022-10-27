d1dddfaf4b1ad4bbf20a3168fb35b1ea015e90f6
Katerine Witkoski <ka.lwitkoski@gmail.com>
2022-10-26 21:34:30 -0300

    Usando condição

--- anotacoes-slides.md
+++ anotacoes-slides.md
@@ -4,3 +4,7 @@ Linguagem Python - Criado por Guido (em 1991).
 
 - variáveis: O que eu quiser guardar durante a execução de um programa eu guardo em 
 uma variável. - Eu não preciso declarar a variável. 
+
+Lógica de programação 
+
+x = (5 + 2) * 10 - parentêses primeiro - camada interna primeiro
--- aulas-praticas-2.py
+++ aulas-praticas-2.py
@@ -7,8 +7,38 @@ nome = input("Digite seu nome: ")
 print(f"Boa noite, {nome}")
 
 # Pede a idade para usuário "Qual sua idade"?
-idade_usuario = input("Qual é a sua idade? ")
+idade_usuario = int(input("Qual é a sua idade? "))
 
 # Exiba "Sua idade é..."
+#print(f"Sua idade é {idade_usuario}")
 
-print(f"Sua idade é {idade_usuario}")
+# O dobro da sua idade é:
+dobro_idade = idade_usuario * 2
+
+print("O dobro da sua idade é {}".format(dobro_idade))
+
+# Estrutura condicional - o famoso "SE"
+# Se a pessoa for maior de ideade, mostre:
+# "Você é maior de idade, ótimo! Já pode beber ou dirigir!"
+
+if idade_usuario >= 18:
+    print("Você é maior de idade, ótimo! Já pode beber ou dirigir!")
+else:
+    print("Volte para casa!") # o else não tem obrigação
+
+# E se eu quisesse perguntar o gênero (M = masculino; F = Feminino)
+# Mostre (...e você também precisa/precisou prestar serviço
+# militar obrigatório)
+
+genero = input("Qual é o seu gênero? M = Masculino, F = Feminino, O = Outros: ")
+
+if idade_usuario >= 18 and genero == "M":
+    print("...e você também precisa/precisou prestar serviço militar obrigatório")
+    #if idade_usuario == 18:
+        #print("Se apresente no quartel") # condição dentro de condição.
+else:
+    print("Você foi liberada do serviço militar obrigatório")
+if idade_usuario < 18:
+        print("Você é uma criança")
+
+# elif (else if)
