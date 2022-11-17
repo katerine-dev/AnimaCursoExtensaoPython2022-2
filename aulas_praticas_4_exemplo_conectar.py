d911c00c50d07e35e98fbd5dd04fc13ae15d20a4
Katerine Witkoski <ka.lwitkoski@gmail.com>
2022-11-16 22:11:42 -0300

    função usando o banco de dados

---
+++ aulas_praticas_4_exemplo_conectar.py
@@ -0,0 +1,18 @@
+# Função - dados no banco
+
+#1o. passo: importar a biblioteca sqlite3
+import sqlite3
+
+
+#PASSOS 2 e 3 VIRARÃO função conectar()
+def conectar():
+  #2o. passo: Vamos estabelecer uma
+  #conexão com o banco de dados
+  conexao = sqlite3.connect("data/dc_universe.db")
+
+  #3o. passo: criar um objeto do tipo cursor
+  cursor = conexao.cursor()
+
+  return conexao, cursor
+
+
---
+++ aulas_praticas_4_exemplo_funcao.py
@@ -0,0 +1,29 @@
+# Importar a função do 'aulas-praticas-4-exemplo-conectar'
+
+import aulas_praticas_4_exemplo_conectar as bd
+
+conexao, cursor = bd.conectar()
+
+nome = input("Informe o nome do herói/vilão: ")
+nome_civil = input("Informe o nome civil do herói/vilão (sua identidade secreta): ")
+tipo_numerico = input("Tecle 1 para Herói(na) ou 2 para Vilã(o): ")
+
+# Consulta para o valor máximo usado no banco
+
+sql = "SELECT MAX(pessoa_id)+1 FROM pessoas" # vai pegar o 12 - "incrementar" - vai pegar a ultima linha e me retornar a próxima
+cursor.execute(sql)
+pessoa_id = cursor.fetchone()[0] #só a primeira coluna
+
+if tipo_numerico == "1":
+    tipo = "Herói(na)"
+else:
+    tipo = "Vilã(o)"
+
+sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
+
+cursor.execute(sql)
+conexao.commit()
+conexao.close()
+
+
+
